f196f962803a74b4c1f06580ae6ed1385a2bc0e7
alex_tim <aleksandr.tim707@gmail.com>
2020-05-20 16:42:31 +0300

    08 - Added jQuery 02

--- app.js
+++ app.js
@@ -1,34 +1,77 @@
 $(function(){
 
+
+    // Fixed header
     let header = $('#header');
     let intro = $('#intro');
-    
     // Высота
-    let introH;
-
+    let introH = intro.innerHeight();
     //Позиция скролла
     let scrollPos = $(window).scrollTop();
 
-    $(window).on('scroll load resize', function() {
+    //Navigations
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
 
+    checkScroll(scrollPos, introH);
+
+    $(window).on('scroll resize', function() {
         // Высота
         introH = intro.innerHeight();
+        //Позиция скролла
         scrollPos = $(this).scrollTop();
 
-        console.log(introH);
+        checkScroll(scrollPos, introH);
 
+        // console.log(introH);
+        // console.log(scrollPos);
+    }); 
+
+    function checkScroll(scrollPos, introH) {
         if(scrollPos > introH) {
             header.addClass('fixed');
         }
-        else
-        {
+        else {
             header.removeClass('fixed');
         }
+    }
 
-        // console.log(scrollPos);
 
-    }); 
+    // Smooth scroll
+    $('[data-scroll]').on('click', function(event) {
+        event.preventDefault();
 
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass('show');
+        
+        // console.log(elementOffset);
+
+        $(' html, body').animate({
+            scrollTop: elementOffset - 50
+        }, 500);
+    });
+    
+    
+    // Nav Toggle
+    $('#navToggle').on('click', function(event) {
+        event.preventDefault();
+
+        nav.toggleClass('show');
+    });
+
+    // Testimonials https://kenwheeler.github.io/slick/
+    let slider = $('#testimonialsSlider');
+
+    slider.slick({
+        infinite: true, 
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
 
 
 }); 
